62f2b32a8d102897c27feebba30f16d660c2c0e0
Andrew Teece <andrewteece@gmail.com>
2023-12-09 14:30:53 -0600

    first commit

---
+++ 01-console/script.js
@@ -0,0 +1,23 @@
+// Log number
+
+
+// Log string
+
+
+// Log multiple values
+
+
+// Log a variable
+
+
+// Console error & warning
+
+
+// Log object as table
+
+
+// Group console commands
+
+
+// Add CSS to logs
+
